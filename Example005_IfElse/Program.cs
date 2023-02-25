50602bda589524c96210c371b0e5555517fbd9a8
El <felmira@mail.ru>
2023-02-25 19:34:36 +0300

    add example005 If Else Mary

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// особенное приветствие
+Console.Write("Ввдите Ваше имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура! Это Маша!");
+}
+else
+{
+    System.Console.Write("Привет! ");
+    System.Console.WriteLine(username);
+}
