892bdda996c9f84cf31ceb7228d914630f32ca70
El <felmira@mail.ru>
2023-02-25 19:16:04 +0300

    add example0031 random sum

---
+++ Example0031_RandomSum/Program.cs
@@ -0,0 +1,7 @@
+﻿// сложение случайных чисел
+int numberA = new Random().Next (1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next (1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+System.Console.WriteLine(result);
