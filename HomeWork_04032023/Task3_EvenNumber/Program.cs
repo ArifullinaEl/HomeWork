d25a75b15dd1a237c8ad261c3e7a34f37e358be0
El <felmira@mail.ru>
2023-02-27 23:23:15 +0300

    did task3

---
+++ HomeWork_04032023/Task3_EvenNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+int even = inputNumber % 2;
+if (even == 0)
+{
+    System.Console.WriteLine("это число четное");
+}
+
+else
+{
+    System.Console.WriteLine("это число нечетное");
+}
+
+
+
