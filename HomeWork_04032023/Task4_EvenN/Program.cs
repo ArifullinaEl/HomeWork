f795578bed968f88df61e625da9a2077ef19c4fc
El <felmira@mail.ru>
2023-02-27 23:52:35 +0300

    did task4

---
+++ HomeWork_04032023/Task4_EvenN/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает 
+// число (N), а на выходе показывает все чётные
+
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i<n-1)
+{
+   i+=2;
+   System.Console.WriteLine(i);
+}
+
+
