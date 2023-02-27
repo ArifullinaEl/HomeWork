f9ceb986f4150b1f4666937bb2d5402bff9534ce
El <felmira@mail.ru>
2023-02-27 21:41:06 +0300

    did task1

---
+++ HomeWork_04032023/Task1_MaxOfTwoNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+int inputNumberA = Convert.ToInt32(Console.ReadLine());
+int inputNumberB = Convert.ToInt32(Console.ReadLine());
+
+if (inputNumberA > inputNumberB)
+{
+    string result = inputNumberA.ToString() + "больше чем" + inputNumberB.ToString();
+    Console.WriteLine(result);
+    
+}
+
+else
+{
+    Console.WriteLine($"{inputNumberB} больше чем {inputNumberA}");
+    
+}
+
