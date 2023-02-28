473dbf135f2ba99baa0b1e8c3e207ca8a0b84454
El <felmira@mail.ru>
2023-03-01 00:01:30 +0300

    did task3 homework05032023

---
+++ HomeWork_05032023/Task2_ThirdDigit/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Введите число");
+int inputNumber = Convert.ToInt32(System.Console.ReadLine());
+
+
+
+Console.WriteLine("Hello, World!");
---
+++ HomeWork_05032023/Task3_Holidays/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+System.Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели");
+int inputNumber = Convert.ToInt32(System.Console.ReadLine());
+
+if (inputNumber >= 1 && inputNumber <= 5) 
+{
+    System.Console.WriteLine("Этот день рабочий");
+}
+
+else
+{
+    if (inputNumber == 6 || inputNumber == 7)
+    {
+        System.Console.WriteLine("Этот день выходной");
+    }
+
+    else
+    {
+        System.Console.WriteLine("Будьте внимательнее, только от 1 до 7");
+    }
+
+}
+
