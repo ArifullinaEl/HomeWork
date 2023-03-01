984401d2971a4849b5f6a1b9443dbfee85cc1fd6
El <felmira@mail.ru>
2023-03-01 23:37:29 +0300

    did task2 homework05032023

--- HomeWork_05032023/Task2_ThirdDigit/Program.cs
+++ HomeWork_05032023/Task2_ThirdDigit/Program.cs
@@ -3,7 +3,15 @@
 
 System.Console.WriteLine("Введите число");
 int inputNumber = Convert.ToInt32(System.Console.ReadLine());
+string inputNumberText = Convert.ToString(inputNumber);
 
+if (inputNumberText.Length>2) 
+{
+    System.Console.WriteLine(inputNumberText[2]);
+}
 
+else
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
 
-Console.WriteLine("Hello, World!");
